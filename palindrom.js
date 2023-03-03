857fdf8b8abb67617a42924f6faa9dd6070d5efe
Victoria G <v.gustei@gmail.com>
2023-03-03 22:51:53 +0200

    change

--- palindrom.js
+++ palindrom.js
@@ -38,8 +38,8 @@
 let palindromNumbers = [];
 function isPalindrom(num){
    let initialNumber = num;
-   let finalNum = 1;
-   while (num != 0) {
+   let finalNum = 0;
+   while (num != 0) { 
       finalNum = num % 10 + finalNum * 10;
       num = Math.floor(num / 10);
    }
